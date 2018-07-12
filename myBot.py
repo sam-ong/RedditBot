5dd448b9031846cf047896d9bffd4c38dd799b80
Sam <samong0105@gmail.com>
2018-07-13 11:39:08 +1200

    Added functionality for the bot to reply to any post that has All Star by SmashMouth lyrics in it with lyrics back

--- myBot.py
+++ myBot.py
@@ -1,6 +1,7 @@
 import praw
 import time
-
+import re
+import os
 
 def authenticate():
     print("Authenticating...")
@@ -9,12 +10,31 @@ def authenticate():
     return reddit
 
 def run_bot(reddit):
-    pass
+    if not os.path.isfile("posts_replied_to.txt"):
+        posts_replied_to = []
+    else:
+        with open("posts_replied_to.txt", "r") as file:
+            posts_replied_to = file.read()
+            posts_replied_to = posts_replied_to.split("\n")
+            posts_replied_to = list(filter(None, posts_replied_to))
+    subreddit = reddit.subreddit("test")
+    for submission in subreddit.hot(limit = 10):
+        if submission.id not in posts_replied_to:
+            if re.search("the years start coming", submission.title, re.IGNORECASE):
+                #reply to the post
+                submission.reply("and they don't stop coming")
+                print('Bot replying to', str(submission.author))
+                #remember the post we have replied to
+                posts_replied_to.append(submission.id)
+    with open("posts_replied_to.txt", "w") as file:
+        for post_id in posts_replied_to:
+            file.write(post_id + "\n")
 
 def main():
     reddit = authenticate()
     while True:
         run_bot(reddit)
+        time.sleep(15)
 
 if __name__ == '__main__':
     main()
