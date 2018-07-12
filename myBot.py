375725acac75736199558a7afe2b09edfce116cd
Sam <samong0105@gmail.com>
2018-07-13 11:18:32 +1200

    Fixed silly errors that made it not compile properly, my bad

--- myBot.py
+++ myBot.py
@@ -2,21 +2,19 @@ import praw
 import time
 
 
-
-
 def authenticate():
     print("Authenticating...")
-    reddit = praw.Reddit(bot1)
+    reddit = praw.Reddit('bot1')
     print("Authenticated as {}".format(reddit.user.me()))
     return reddit
 
 def run_bot(reddit):
-    
+    pass
 
 def main():
     reddit = authenticate()
     while True:
         run_bot(reddit)
 
-if name == '__main__':
+if __name__ == '__main__':
     main()
